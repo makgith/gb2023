e92abe6066c0a6581e9bc5735b034fe133df4127
MAK-PC\mak <maktempgma@gmail.com>
2023-07-01 18:25:18 +0300

    Lesson 2 homework

---
+++ 3_intro_to_programming_languages/seminars/1/lesson2/hw2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Input number: ");
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < 3; i++)
+{
+
+}
+
+num = num < 0 ? -num: num;
+
+if (num < 99)
+{
+    Console.WriteLine("There is no 3rd digit in the number!");
+}
+else
+{
+    num = num / 100 % 10;
+    Console.WriteLine($"3rd digit of the number is: {num}");
+}
